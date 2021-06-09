0250c56a52b033fc855b9e4cef713f69eb8bbc22
Houssam Benhim <benhim.houssam@gmail.com>
2021-06-09 12:48:58 +0200

    Initial Project Add

---
+++ src/test/java/com/springframework/passionfruits/PassionFruitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.passionfruits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassionFruitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
