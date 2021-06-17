781b30ef1629ebccefa258823c638ddd69a96301
Houssam Benhim <benhim.houssam@gmail.com>
2021-06-17 10:26:51 +0200

    added ProductController Calss Closes #13

--- passion-fruits-web/src/main/java/com/springframework/passionfruits/controllers/ProductController.java
+++ passion-fruits-web/src/main/java/com/springframework/passionfruits/controllers/ProductController.java
@@ -1,16 +1,27 @@
 package com.springframework.passionfruits.controllers;
 
+import java.util.Set;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springframework.passionfruits.moddels.Product;
+import com.springframework.passionfruits.services.ProductService;
 
 @RestController
 public class ProductController {
 
-	@RequestMapping({"","/","products"})
-	public Product getAllProduct() {
-		
-		return null;
+	private final ProductService productService;
+	
+	
+	public ProductController(ProductService productService) {
+		this.productService = productService;
+	}
+
+
+	@RequestMapping({"","/","/products"})
+	public Set<Product> getAllProduct() {
+		Set<Product> listProduct =  productService.findAll();
+		return listProduct;
 	}
 }
