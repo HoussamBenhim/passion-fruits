a4ab20b46c8f5d372f993d2b828a96c59770b555
Houssam Benhim <benhim.houssam@gmail.com>
2021-08-25 22:48:34 +0200

    ProductCategory Spring Data JPA Service implementation

--- passion-fruits-data/src/main/java/com/springframework/passionfruits/repositories/ProductCategoryRepository.java
+++ passion-fruits-data/src/main/java/com/springframework/passionfruits/repositories/ProductCategoryRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import com.springframework.passionfruits.moddels.ProductCategory;
 
 public interface ProductCategoryRepository extends CrudRepository<ProductCategory, Long> {
-
+	ProductCategory findByCategoryName(String name);
 }
---
+++ passion-fruits-data/src/main/java/com/springframework/passionfruits/springdatajpa/ProductCategorySDJPAService.java
@@ -0,0 +1,50 @@
+package com.springframework.passionfruits.springdatajpa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.springframework.passionfruits.moddels.ProductCategory;
+import com.springframework.passionfruits.repositories.ProductCategoryRepository;
+import com.springframework.passionfruits.services.ProductCategoryService;
+
+public class ProductCategorySDJPAService implements ProductCategoryService {
+	private final ProductCategoryRepository productCategoryRepository;
+
+	public ProductCategorySDJPAService(ProductCategoryRepository productCategoryRepository) {
+		super();
+		this.productCategoryRepository = productCategoryRepository;
+	}
+
+	@Override
+	public Set<ProductCategory> findAll() {
+		Set<ProductCategory> productCats = new HashSet<>();
+		productCategoryRepository.findAll().forEach(productCats::add);
+		return productCats;
+	}
+
+	@Override
+	public ProductCategory findById(Long id) {
+		return productCategoryRepository.findById(id).orElse(null);
+	}
+
+	@Override
+	public ProductCategory save(ProductCategory entity) {
+		return productCategoryRepository.save(entity);
+	}
+
+	@Override
+	public void delete(ProductCategory entity) {
+		productCategoryRepository.delete(entity);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		productCategoryRepository.deleteById(id);
+	}
+
+	@Override
+	public ProductCategory findByName(String name) {
+		return productCategoryRepository.findByCategoryName(name);
+	}
+	
+}
