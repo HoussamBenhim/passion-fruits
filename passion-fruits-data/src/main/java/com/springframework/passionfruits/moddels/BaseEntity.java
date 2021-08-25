30ac31408fad90a0e4b1d3f861408aae2e575bea
Houssam Benhim <benhim.houssam@gmail.com>
2021-08-25 15:40:26 +0200

    BaseEntity JPA annotation

--- passion-fruits-data/src/main/java/com/springframework/passionfruits/moddels/BaseEntity.java
+++ passion-fruits-data/src/main/java/com/springframework/passionfruits/moddels/BaseEntity.java
@@ -2,8 +2,16 @@ package com.springframework.passionfruits.moddels;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class BaseEntity implements Serializable {
- private Long id;
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
 
 public Long getId() {
 	return id;
