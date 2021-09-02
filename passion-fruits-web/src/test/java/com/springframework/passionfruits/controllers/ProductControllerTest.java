4a5c049e77908ec038a7fe34b34067d88aab59c6
Houssam Benhim <benhim.houssam@gmail.com>
2021-09-02 12:53:48 +0200

    fixing productController Test

--- passion-fruits-web/src/test/java/com/springframework/passionfruits/controllers/ProductControllerTest.java
+++ passion-fruits-web/src/test/java/com/springframework/passionfruits/controllers/ProductControllerTest.java
@@ -3,6 +3,7 @@ package com.springframework.passionfruits.controllers;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -19,6 +20,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -40,7 +42,7 @@ class ProductControllerTest {
 	void setUp() throws Exception {
 		products = new HashSet<>();
 		products.add(Product.builder().id(1L).name("banane").build());
-		products.add(Product.builder().id(2L).name("pomme").build());
+		products.add(Product.builder().id(2L).name("banane").build());
 		mockMvc = MockMvcBuilders.standaloneSetup(productController).build();
 	}
 
@@ -49,7 +51,7 @@ class ProductControllerTest {
 
 		when(productService.findAll()).thenReturn(products);
 		mockMvc.perform(get("/api/products"))
-					.andExpect(status().isOk()).andExpect(jsonPath("$[0].name", is("pomme")));
+					.andExpect(status().isOk()).andExpect(jsonPath("$[0].name", is("banane")));
 
 	}
 
